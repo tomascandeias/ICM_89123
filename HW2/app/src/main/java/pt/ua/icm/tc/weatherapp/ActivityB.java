d7ee2c84520987c8297c75a4dff06eede4253735
Tomás Candeias <tomaslcandeias@outlook.com>
2022-05-25 00:08:19 +0100

    hw2

--- HW2/app/src/main/java/pt/ua/icm/tc/weatherapp/ActivityB.java
+++ HW2/app/src/main/java/pt/ua/icm/tc/weatherapp/ActivityB.java
@@ -1,9 +1,7 @@
 package pt.ua.icm.tc.weatherapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class ActivityB extends AppCompatActivity {
@@ -27,35 +25,5 @@ public class ActivityB extends AppCompatActivity {
         fragmentB2.setArguments(bundle);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_view, fragmentB).commit();
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.cities, new FragmentA());
-        getSupportFragmentManager().beginTransaction().replace(R.id.details, fragmentB2);
-
     }
-    /*
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        System.err.println("hey");
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            System.err.println("hey");
-            Bundle bundle = new Bundle();
-            bundle.putString("name", cityName);
-
-            FragmentA fragmentA = new FragmentA();
-            fragmentA.setArguments(bundle);
-
-            setContentView(R.layout.fragment_layout);
-            getSupportFragmentManager().beginTransaction().replace(R.id.cities, fragmentA).commit();
-            getSupportFragmentManager().beginTransaction().replace(R.id.details, fragmentB).commit();
-
-        }
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("name", cityName);
-    }
-     */
 }
